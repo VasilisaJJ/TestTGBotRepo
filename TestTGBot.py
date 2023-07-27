05d04045b181a5b098477e1a830cae0e36fa25c9
Vasilisa_jj <kvs14t419@gmail.com>
2023-07-27 17:57:44 +0400

    Additional mp3 files with eng titles

--- TestTGBot.py
+++ TestTGBot.py
@@ -40,7 +40,9 @@ def handle_text(message):
     if message.text == "/other":
         bot.send_message(message.chat.id, 'Остальные варианты', reply_markup=create_keyboard_other())
     elif message.text == 'Про GPT':
+        audio_url_gpt = 'https://github.com/VasilisaJJ/TestTGBotRepo/blob/main/ПроGPT.mp3'
         bot.send_message(message.chat.id, "Вы выбрали послушать объяснения для Бабули про GPT")
+        bot.send_audio(message.chat.id, audio_url_gpt)
     elif message.text == 'Про разницу между SQL и NoSQL':
         bot.send_message(message.chat.id, "Вы выбрали послушать объяснения для Бабули про разницу между SQL и NoSQL")
     elif message.text == 'Про первую любовь':
@@ -50,22 +52,22 @@ def handle_text(message):
                          "Вы выбрали предыдущие варианты",
                          reply_markup=create_keyboard_start())
     elif message.text == 'Селфи':
-        image_url_1 = 'https://sun9-79.userapi.com/impg/bmsbrWA-3K3b8ekiyi0Tiz0UgYGhYzGJ1h44Pg/KJHXu5KSVLM.jpg?size=1620x2160&quality=96&sign=ba35ea6c2ce041c5a5c22efd76a35213&type=album'
+        image_url_1 = 'https://raw.githubusercontent.com/VasilisaJJ/TestTGBotRepo/main/%D1%84%D0%BE%D1%82%D0%BE%20%D1%81%D0%B5%D0%B9%D1%87%D0%B0%D1%81.jpg'
         bot.send_photo(message.chat.id, image_url_1, caption="Вы выбрали посмотреть последнее селфи Василисы")
         #bot.send_message(message.chat.id, "Вы выбрали посмотреть последнее селфи Василисы")
     elif message.text == 'Школьное фото':
-        image_url_2 = 'https://sun9-69.userapi.com/impf/c9268/v9268350/754/0glEpMt3FCo.jpg?size=1280x851&quality=96&sign=a3fb494c2bca27f471c61201c566c702&type=album'
+        image_url_2 = 'https://raw.githubusercontent.com/VasilisaJJ/TestTGBotRepo/main/%D0%A8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5%20%D1%84%D0%BE%D1%82%D0%BE.jpg'
         bot.send_photo(message.chat.id, image_url_2, caption="Вы выбрали посмотреть школьное фото Василисы")
         #bot.send_message(message.chat.id, "Вы выбрали посмотреть школьное фото Василисы")
     elif message.text == 'Увлечения':
-        hobbies = "Меня зовут Василиса, мне 27 лет. Моё главное увлечение - это искусство!" \
+        hobbies = "Меня зовут Василиса, мне 27 лет. Моё главное увлечение - это искусство! \n" \
                    "Я художник. Я пишу картины и создаю арт-игры \n" \
                    "Я считаю, что игры - это новый медиум в искусстве. \n" \
                    "Они предоставляют огромный спектр инструментов для донесения художественного видения до зрителя.\n" \
-                   "Я стараюсь популяризировать арт-игры, участвую в многих арт-резиденциях и работаю с грантами. \n" \
+                   "Я стараюсь популяризировать арт-игры, участвую во многих арт-резиденциях и работаю с грантами. \n" \
                    "Главной темой моего искусства является Потустороннее и Непознанное - связь нашего Сознания и Подсознания"
 
-        bot.send_message(message.chat.id, "Вы выбрали прочесть про увлечения Василисы: ", hobbies, parse_mode='Markdown')
+        bot.send_message(message.chat.id, hobbies, parse_mode='Markdown')
     else:
         bot.send_message(message.chat.id, "Выберите один из предложенных вариантов или другие варианты, написав \"/other\"", 
                          reply_markup=create_keyboard_start())
